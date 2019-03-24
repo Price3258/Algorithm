1c309736000ec0b8fb1e37bc8ca57dd712321c5e
wtkim <rladnjsxo123@naver.com>
2019-03-24 12:23:58 +0900

    edit best-time-to-buy-and-sell-stock

--- test/reetcode/BestTimeToBuyAndSellStock.java
+++ test/reetcode/BestTimeToBuyAndSellStock.java
@@ -4,6 +4,16 @@ import org.hamcrest.core.Is;
 import org.junit.Assert;
 import org.junit.Test;
 
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+
+ */
+
 public class BestTimeToBuyAndSellStock {
 
     @Test
