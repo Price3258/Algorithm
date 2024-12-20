98dcab8ada0e9d34b4ab70c276ff4419d768fc2f
김원태 <rladnjsxo123@naver.com>
2024-12-20 20:49:26 +0900

    betterHash

--- src/lib/HashTable.js
+++ src/lib/HashTable.js
@@ -2,19 +2,38 @@ function HashTable() {
     this.table = new Array(137);
     this.simpleHash = simpleHash;
     this.showDistro = showDistro;
+    this.batterHash = batterHash;
     this.put = put;
 }
 
 function simpleHash(data) {
     var total = 0;
-    for( var i =0; i< data.length; i++) {
+    for( var i =0; i< data.length; ++i) {
         total += data.charCodeAt(i);
     }
     return total % this.table.length;
 }
 
+// 호너의 메소드 사용. 
+// 결과에 소수를 곱하는 과정을 추가
+// 
+function batterHash(string) {
+    const H = 37;
+    var total = 0;
+    for( var i =0; i< string.length; ++i) {
+        total += H * total + string.charCodeAt(i);
+    }
+    total = total % this.table.length;
+    if(total < 0) {
+        total += this.table.length -1;
+    }
+
+    return parseInt(total);
+}
+
+
 function put(data) {
-    var pos = this.simpleHash(data);
+    var pos = this.batterHash(data);
     this.table[pos] = data;
 }
 
@@ -26,7 +45,8 @@ function showDistro() {
     }
 }
 
-var someNames = ["David","Jenn","John", " Donnie", "Raymond","Mike"]
+
+var someNames = ["David", "Jenn", "John", "Donnie", "Raymond", "Mike", "Clayton"]
 
 var hTable = new HashTable();
 
