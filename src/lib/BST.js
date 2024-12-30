85865eb542b2e0cc3f2dc25a40fe1d07fc3e53b0
김원태 <rladnjsxo123@naver.com>
2024-12-30 14:44:48 +0900

    Create BST.js

---
+++ src/lib/BST.js
@@ -0,0 +1,44 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    show() {
+        return this.data;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(data) {
+        let newNode = new Node(data);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+           let current = this.root;
+           let parent;
+           while (true) {
+               parent = current;
+               if (data < current.data) { // 현재 데이터보다 작으면 왼쪽
+                   current = current.left;
+                   if (current === null) {
+                       parent.left = newNode;
+                       break;
+                   }
+               } else {
+                   current = current.right; // 크면 오른쪽
+                   if (current === null) {
+                       parent.right = newNode;
+                       break;
+                   }
+               }
+           }
+        }
+    }
+}
+
