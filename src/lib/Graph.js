5cc167cb278a65c05d5d0020876a5fafcef650fd
김원태 <rladnjsxo123@naver.com>
2025-01-03 23:32:50 +0900

    Update Graph.js

--- src/lib/Graph.js
+++ src/lib/Graph.js
@@ -1,27 +1,40 @@
 class Graph {
     constructor(v) {
-       this.vertices = v;
-       this.edges = 0;
-       this.adj = [];
-         for (let i = 0; i < this.vertices; i++) {
-              this.adj[i] = [];
-              this.adj[i].push("");
-         }
+        this.vertices = v;
+        this.edges = 0;
+        this.adj = [];
+        for (let i = 0; i < this.vertices; i++) {
+            this.adj[i] = [];
+            this.adj[i].push("");
+        }
+        this.marked = [];
     }
     addEdge(v, w) {
-       this.adj[v].push(w);
-       this.adj[w].push(v);
-       this.edges++;
+        this.adj[v].push(w);
+        this.adj[w].push(v);
+        this.edges++;
     }
 
     showGraph() {
-       for (let i = 0; i < this.vertices; i++) {
+        for (let i = 0; i < this.vertices; i++) {
           console.log(i + " -> ");
           for (let j = 0; j < this.vertices; j++) {
-             if (this.adj[i][j])
+            if (this.adj[i][j])
                 console.log(this.adj[i][j] + ' ');
           }
        }
+    }
+
+    dfs(v) {
+        this.marked[v] = true;
+        if (this.adj[v] != undefined) {
+            console.log("Visited vertex: " + v);
+        }
+        for (let w in this.adj[v]) {
+            if (!this.marked[w]) {
+                this.dfs(w);
+            }
+        }
     }
 }
 
