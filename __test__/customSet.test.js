6d815e489346c23141d682fc4dc8c66c9b60b5ac
김원태 <rladnjsxo123@naver.com>
2025-01-03 13:26:12 +0900

    Update customSet.test.js

--- __test__/customSet.test.js
+++ __test__/customSet.test.js
@@ -11,3 +11,28 @@ it("custom set test", () => {
     expect(setA.isSubsetOf(setB)).toBe(false);
  
 })
+
+it("normal set test",()=> {
+    const a = new Set([1, 2, 3]);
+    const b = new Set([2, 3, 4]);
+
+    // 합집합
+    const union = new Set([...a, ...b]);
+    expect(union).toStrictEqual(new Set([1,2,3,4]));
+
+    // 교집합
+    const intersection = new Set([...a].filter(x => b.has(x)));
+    expect(intersection).toStrictEqual(new Set([2,3]));
+
+
+    // 차집합
+    const difference = new Set([...a].filter(x => !b.has(x)));
+    expect(difference).toStrictEqual(new Set([1]));
+
+
+    // 대칭차집합
+    const symmetricDifference = new Set(
+    [...a].filter(x => !b.has(x)).concat([...b].filter(x => !a.has(x)))
+    );
+    expect(symmetricDifference).toStrictEqual(new Set([1,4]));
+})
