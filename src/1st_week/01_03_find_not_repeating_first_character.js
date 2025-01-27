6e5c5f484d60f2d3d51ae911d227c6c05f98f5e1
김원태 <rladnjsxo123@naver.com>
2025-01-27 17:51:38 +0900

    Create 01_03_find_not_repeating_first_character.js

---
+++ src/1st_week/01_03_find_not_repeating_first_character.js
@@ -0,0 +1,28 @@
+/*
+Q. 다음과 같이 영어로 되어 있는 문자열이 있을 때, 이 문자열에서 반복되지 않는 첫번째 문자를 반환하시오. 만약 그런 문자가 없다면 _ 를 반환하시오.
+"abadabac" # 반복되지 않는 문자는 d, c 가 있지만 "첫번째" 문자니까 d를 반환해주면 됩니다!
+*/
+
+function solution(arr) {
+  const table = {};
+  for (const char of arr) {
+    if (table[char]) {
+      table[char] += 1;
+    } else {
+      table[char] = 1;
+    }
+  }
+  tableKeys = Object.keys(table);
+
+  for (key of tableKeys) {
+    if (table[key] == 1) {
+      return key;
+    }
+  }
+
+  return "_";
+}
+
+console.log("정답 = d 현재 풀이 값 =", solution("abadabac"));
+console.log("정답 = c 현재 풀이 값 =", solution("aabbcddd"));
+console.log("정답 =_ 현재 풀이 값 =", solution("aaaaaaaa"));
