cdf76b5e9d671b5ecd53d6a9a019500b7f907c5d
김원태 <rladnjsxo123@naver.com>
2025-02-04 19:15:30 +0900

    refactor: max heap

--- src/lib/MaxHeap.js
+++ src/lib/MaxHeap.js
@@ -8,6 +8,20 @@ class MaxHeap {
     this._heapifyUp();
   }
 
+  pop() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+    const max = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this._heapifyDown();
+
+    return max;
+  }
+
   _heapifyUp() {
     let index = this.heap.length - 1;
     while (index > 0) {
@@ -22,6 +36,29 @@ class MaxHeap {
       index = parentIndex;
     }
   }
+
+  _heapifyDown() {
+    let index = 0;
+    while (index * 2 + 1 < this.heap.length) {
+      let leftChild = index * 2 + 1;
+      let rightChild = index * 2 + 2;
+      let largerChild = leftChild;
+      if (
+        rightChild < this.heap.length &&
+        this.heap[rightChild] > this.heap[leftChild]
+      ) {
+        largerChild = rightChild;
+      }
+      if (this.heap[index] >= this.heap[largerChild]) {
+        break;
+      }
+      [this.heap[index], this.heap[largerChild]] = [
+        this.heap[largerChild],
+        this.heap[index],
+      ];
+      index = largerChild;
+    }
+  }
 }
 
 const maxHeap = new MaxHeap();
@@ -32,6 +69,6 @@ console.log(maxHeap.heap);
 maxHeap.push(5);
 console.log(maxHeap.heap);
 
-// console.log(maxHeap.pop()); // 10 (최대값)
-// console.log(maxHeap.pop()); // 5
-// console.log(maxHeap.pop()); // 3
+console.log(maxHeap.pop()); // 10 (최대값)
+console.log(maxHeap.pop()); // 5
+console.log(maxHeap.pop()); // 3
